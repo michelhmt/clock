2dd822e2f8a0dd814778dd374d5b83c8102faee0
michelhmt <136892274+michelhmt@users.noreply.github.com>
2025-06-05 12:23:53 +0900

    Update index.js

--- index.js
+++ index.js
@@ -74,14 +74,14 @@ function getRandomInt(max) {//引数maxで指定した数までのランダム�
 function timeRedraw() {
     //console.log(dt.getMinutes()+":"+dt.getSeconds());
 
-    /*
+    
     //水分補給アラートをON
     if(dt.getSeconds() == 0 && dt.getMinutes() == 11){  waterChargeAlartOn();}
     if(dt.getSeconds() == 0 && dt.getMinutes() == 32){  waterChargeAlartOn();}
     
     if(dt.getSeconds() == 0 && dt.getMinutes() == 14){ waterChargeAlartOff();}
     if(dt.getSeconds() == 0 && dt.getMinutes() == 35){ waterChargeAlartOff();}
-    */
+    
 
 
 
